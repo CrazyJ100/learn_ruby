2620bdd67c6ceb6e8cde01007401ad066846de0f
Jeremy White <patch3s93@gmail.com>
2018-10-18 11:44:54 -0400

    ex13.rb with commented out code to work as the lesson intended. Uncomment the code and works with the modifications. Added ex14.rb, ex15.rb and ex15_sample.txt

--- ex13.rb
+++ ex13.rb
@@ -1,5 +1,8 @@
 first, second, third = ARGV
 
 puts "Your first variable is: #{first}"
+# first = gets.chomp
 puts "Your second variable is: #{second}"
+# second = gets.chomp
 puts "Your third variable is: #{third}"
+# third = gets.chomp
